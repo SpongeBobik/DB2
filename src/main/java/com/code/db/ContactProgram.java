567330aea59b6be86402e0e1954c7da33c57024a
SpongeBobik <kulikov.kolay33@yandex.ru>
2022-10-12 22:09:19 +0300

    second try

--- src/main/java/com/code/db/ContactProgram.java
+++ src/main/java/com/code/db/ContactProgram.java
@@ -1,2 +1,50 @@
-package com.code.db;public class ContactProgram {
+package com.code.db;
+
+import java.sql.*;
+
+
+public class ContactProgram {
+    public static void main(String[] args){
+        String jdbcURL="jdbc:postgresql://localhost:5432/ProbaDB";
+        String username = "postgres";
+        String password = "admin";
+
+        try {
+            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+            System.out.println("Connected to PosgreSQL server");
+
+            //Запросы
+            String sql = "INSERT INTO \"Students\" (\"First_name\", \"Second_name\") VALUES (?, ?)";
+            String DELETE = "DELETE FROM \"Students\" WHERE \"id\"=?";
+
+            // Функция удаления
+            PreparedStatement statementDel = connection.prepareStatement(DELETE);
+            statementDel.setInt(1, 3);
+            statementDel.executeUpdate();
+
+            int rowsDel = statementDel.executeUpdate();
+            if (rowsDel>0){
+                System.out.println("Удалено " + rowsDel + " записей");
+            }
+            //Функция добавления
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setString(1, "Иван");
+            statement.setString(2,"Иванов");
+
+            int rows = statement.executeUpdate();
+            if (rows>0) {
+                System.out.println("Было изменено " + rows + " записей.");
+            }
+
+            connection.close();
+
+        } catch (SQLException e) {
+            System.out.println("Error in connection to PostgreSQL server");
+            e.printStackTrace();
+
+        }
+
+
+    }
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,8 @@
 module com.code.db {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires org.postgresql.jdbc;
+    requires java.sql;
 
     opens com.code.db to javafx.fxml;
     exports com.code.db;
